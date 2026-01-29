b117b8de740f3ef19d9b3ed04dbd6f701822129f
jérôme boileux <jerome.boileux@ocobo.co>
2026-01-29 14:19:37 +0100

    chore: update speaker role

--- App.tsx
+++ App.tsx
@@ -1,36 +1,35 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Masterclasses from './components/Masterclasses'
-import Location from './components/Location';
-import Registration from './components/Registration';
-import Footer from './components/Footer';
-import Concierge from './components/Concierge';
-import ComingSoon from './components/ComingSoon';
-import Speakers from './components/Speakers';
-import Agenda from './components/Agenda';
+import type React from "react";
+import Agenda from "./components/Agenda";
+import ComingSoon from "./components/ComingSoon";
+import Concierge from "./components/Concierge";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Location from "./components/Location";
+import Navbar from "./components/Navbar";
+import Registration from "./components/Registration";
+import Speakers from "./components/Speakers";
 
 const App: React.FC = () => {
-  const showComingSoon = import.meta.env.VITE_SHOW_COMING_SOON === 'true';
+	const showComingSoon = import.meta.env.VITE_SHOW_COMING_SOON === "true";
 
-  if (showComingSoon) {
-    return <ComingSoon />;
-  }
+	if (showComingSoon) {
+		return <ComingSoon />;
+	}
 
-  return (
-    <div className="mesh-gradient text-slate-900 min-h-screen font-sans selection:bg-blue-500 selection:text-white">
-      <Navbar />
-      <main>
-        <Hero />
-        <Speakers />
-        <Agenda />
-        <Location />
-        <Registration />
-      </main>
-      <Footer />
-      <Concierge />
-    </div>
-  );
+	return (
+		<div className="mesh-gradient text-slate-900 min-h-screen font-sans selection:bg-blue-500 selection:text-white">
+			<Navbar />
+			<main>
+				<Hero />
+				<Speakers />
+				<Agenda />
+				<Location />
+				<Registration />
+			</main>
+			<Footer />
+			<Concierge />
+		</div>
+	);
 };
 
 export default App;
